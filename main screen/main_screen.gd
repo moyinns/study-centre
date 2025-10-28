243197ff1bd79b3659192af21a5f9fa54635fd9a
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2025-10-28 12:56:53 +0000

    starting to add the todo list

--- main screen/main_screen.gd
+++ main screen/main_screen.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+@onready var todo_list = $"todo list"
+@onready var add_task = $"add task"
+var todo_item_scene = preload("res://main screen/todo_item.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	add_task.pressed.connect(_on_add_task_pressed)
 
+func _on_add_task_pressed():
+	# instantiate the template
+	var new_item = todo_item_scene.instantiate()
+	
+	# add it as a child of TodoList (VBoxContainer)
+	todo_list.add_child(new_item)
+	
+	# optional: set anchors so it stretches properly
+	new_item.anchor_left = 0
+	new_item.anchor_right = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
